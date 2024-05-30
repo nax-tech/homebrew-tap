f8f1c0bdf650dae6222bd1165f846d675517b5c1
Ali Samji <ali.samji@naxgrp.com>
2024-05-30 14:53:34 -0500

    Update to nax 0.1.15.

--- Formula/nax.rb
+++ Formula/nax.rb
@@ -1,19 +1,19 @@
 class Nax < Formula
   desc "Command-line interface for the NAX platform"
   homepage "naxgrp.com"
-  version "0.1.13"
+  version "0.1.15"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://naxpublicstuffs.blob.core.windows.net/binaries/nax-0.1.13/nax-0.1.13-darwin-arm64.zip"
-      sha256 "160fa5be4eec0aac19f0c838c54f5a45a125bad7c99a4db639a16d4905619183"
+      url "https://naxpublicstuffs.blob.core.windows.net/binaries/nax-0.1.15/nax-0.1.15-darwin-arm64.zip"
+      sha256 "eace178f232884ca50188a6838878a25b6537195bf3ce2b6a270a70dd286c467"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://naxpublicstuffs.blob.core.windows.net/binaries/nax-0.1.13/nax-0.1.13-linux-x86_64.zip"
-      sha256 "5bcea0da5693160c9db9fc6a2776fdfd2ac7644bfa54cc0ec8531183f0c92615"
+      url "https://naxpublicstuffs.blob.core.windows.net/binaries/nax-0.1.15/nax-0.1.15-linux-x86_64.zip"
+      sha256 "2992a33e196e3aaf099e6196d2c7e7eb4c11a279a2c82b58333f70bea799fdfe"
     end
   end
 
@@ -36,10 +36,8 @@ class Nax < Formula
 
   def caveats
     <<~EOS
-
       If you previously installed nax-cli via the cargo command, you may need to uninstall it with `cargo uninstall nax`
       to ensure that the nax binary you installed with Homebrew is used instead.
-
     EOS
   end
 end
