92c847d342e03b70319f99e90e3a5fb986e67f39
Ali Samji <ali.samji@naxgrp.com>
2024-05-31 14:25:41 -0500

    Update nax.rb to 0.1.16

--- Formula/nax.rb
+++ Formula/nax.rb
@@ -1,25 +1,28 @@
 class Nax < Formula
   desc "Command-line interface for the NAX platform"
   homepage "naxgrp.com"
-  version "0.1.15"
-
-  on_macos do
-    if Hardware::CPU.arm?
-      url "https://naxpublicstuffs.blob.core.windows.net/binaries/nax-0.1.15/nax-0.1.15-darwin-arm64.zip"
-      sha256 "eace178f232884ca50188a6838878a25b6537195bf3ce2b6a270a70dd286c467"
-    end
-  end
+  version "0.1.16"
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://naxpublicstuffs.blob.core.windows.net/binaries/nax-0.1.15/nax-0.1.15-linux-x86_64.zip"
-      sha256 "2992a33e196e3aaf099e6196d2c7e7eb4c11a279a2c82b58333f70bea799fdfe"
+      url "https://naxpublicstuffs.blob.core.windows.net/binaries/nax-0.1.16/nax-0.1.16-linux-x86_64.zip"
+      sha256 "38656e343842af92e6c4c2c8adb446ff58cdb7caae0bdab18bbb5fd21bf89788"
+    end
+  end
+  
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://naxpublicstuffs.blob.core.windows.net/binaries/nax-0.1.16/nax-0.1.16-darwin-arm64.zip"
+      sha256 "14834bea014d78c49b033e7142384ec00a7323de61fc6227ca71f9360420cfaf"
+    else
+      url "https://naxpublicstuffs.blob.core.windows.net/binaries/nax-0.1.16/nax-0.1.16-darwin-x86_64.zip"
+      sha256 "50462d2e651f4b944a14a98c8d5a105c0955d33930a1735aba86325839d01c1c"
     end
   end
 
-  depends_on "awscli"
-  depends_on "azure-cli"
-  depends_on "git"
+  # depends_on "awscli"
+  # depends_on "azure-cli"
+  # depends_on "git"
 
   livecheck do
     url "https://naxpublicstuffs.blob.core.windows.net/binaries?comp=list&restype=container"
